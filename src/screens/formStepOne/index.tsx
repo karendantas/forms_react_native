150dfb2694ff5b8f4a9dd415c29156740927e8f9
Karen Dantas <karennobre74@gmail.com>
2024-11-04 09:06:57 -0300

    first commit

---
+++ src/screens/formStepOne/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { styles } from "./styles";
+
+export function FormStepOne(){
+    return (
+        <View style = {styles.container}>
+
+        </View>
+    )
+}
